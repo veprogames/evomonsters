fd0bd17623830670c0998e69dfa8c39d37405131
veprogames <veprogames.gamedev@gmail.com>
2022-11-05 16:07:24 +0100

    some layout

--- src/App.svelte
+++ src/App.svelte
@@ -7,13 +7,22 @@
     $game.currentMeal = Meal.get(0);
 </script>
 
-<h1 class="text-green-400 text-4xl p-8">Calories: {F($game.calories.amount, 2)}</h1>
-<p>Your highest is {F($game.calories.highest, 2)}</p>
-<p>Your total is {F($game.calories.total, 2)}</p>
-
-<MealCard meal={$game.currentMeal}/>
-
-<p>Size: {$game.monster.size}m</p>
-<footer class="fixed bottom-0 w-full">
+<header class="bg-slate-400 dark:bg-slate-700 shadow-md p-4 flex justify-evenly">
+    <h1>Evomonsters</h1>
+    <span>Calories: {F($game.calories.amount, 2)}</span>
+</header>
+<main class="p-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 grid-rows-2 gap-4">
+        <p>Size: {$game.monster.size}m</p>
+        <div class="flex justify-center">
+            <MealCard meal={$game.currentMeal}/>
+        </div>
+        <div>
+            <p>Your highest is {F($game.calories.highest, 2)}</p>
+            <p>Your total is {F($game.calories.total, 2)}</p>
+        </div>
+    </div>
+</main>
+<footer class="bg-slate-400 dark:bg-slate-700 p-4 fixed bottom-0 w-full">
     Version {$game.version}
 </footer>
