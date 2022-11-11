87195824d892a012b97a3e553a6b8ec8c036580e
veprogames <veprogames.gamedev@gmail.com>
2022-11-11 13:37:33 +0100

    formatting sizes

--- src/lib/components/MonsterCard.svelte
+++ src/lib/components/MonsterCard.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import type Monster from "../class/Monster";
-    import { F } from "../format";
+    import { F, FSize } from "../format";
     import { game } from "../stores";
 
     export let monster: Monster;
@@ -11,7 +11,6 @@
 <div class="card flex flex-col items-center gap-1">
     <img src="/images/placeholder.png" alt="Monster" class="w-24 h-24 lg:w-36 lg:h-36"/>
     <p class="font-semibold">Young Monster</p>
-    <p>Size: {F(monster.size, 2, 2)}m</p>
-    <p>Mass: {F(monster.mass, 2, 3)}u</p>
+    <p>Size: {FSize(monster.size, 2, 2)}</p>
     <p>Bite Dmg: {F(biteDamage)}</p>
 </div>
--- src/lib/format.ts
+++ src/lib/format.ts
@@ -1,11 +1,32 @@
-import Decimal from "break_infinity.js";
+import Decimal, { type DecimalSource } from "break_infinity.js";
 import * as ADNotations from "@antimatter-dimensions/notations";
 
 const notation = new ADNotations.StandardNotation();
 
-export function F(n: Decimal|string|number, places: number = 2, places1000: number = 0): string{
+const distanceUnits: Array<[number, string]> = [
+    [0.01, "cm"],
+    [1, "m"],
+    [12_756_000, "Earths"],
+    [138_346_500, "Jupiters"],
+    [1_392_684_000, "Suns"]
+]
+
+export function F(n: DecimalSource, places: number = 2, places1000: number = 0): string{
     if(typeof n === "string" || typeof n === "number"){
         n = new Decimal(n);
     }
     return notation.format(n, places, places1000);
+}
+
+export function FSize(distance: DecimalSource, places: number = 2, places1000: number = 2){
+    if(!(distance instanceof Decimal)){
+        distance = new Decimal(distance);
+    }
+    let i = 0;
+    for(const [size, name] of distanceUnits){
+        const nextSize = distanceUnits[++i][0];
+        if(distance.gte(size) && distance.lt(nextSize)){
+            return `${F(distance.div(size), places, places1000)} ${name}`;
+        }
+    }
 }
