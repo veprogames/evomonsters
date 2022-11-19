58d7405986a700a411975938a387eca4fd5cafd4
veprogames <veprogames.gamedev@gmail.com>
2022-11-19 17:09:45 +0100

    make it obvious when Achievement is unlocked

--- src/lib/components/AchievementComp.svelte
+++ src/lib/components/AchievementComp.svelte
@@ -7,10 +7,20 @@
 </script>
 
 <div class="relative inline-flex justify-center items-center
-    w-20 h-20 bg-slate-200 dark:bg-slate-700 rounded-md group">
+    w-20 h-20 bg-slate-200 dark:bg-slate-700 rounded-md group"
+    class:unlocked={achievement.unlocked}>
     <img src={icon} alt="Achievement Icon" class="w-16 h-16 rounded-md"/>
     <Tooltip>
         <p class="text-blue-400 font-bold">{achievement.title}</p>
         <p>{achievement.description}</p>
+        {#if achievement.unlocked}
+            <p class="text-green-400 font-bold">Unlocked!</p>
+        {/if}
     </Tooltip>
 </div>
+
+<style lang="postcss">
+    div.unlocked{
+        @apply bg-slate-100 dark:bg-slate-500;
+    }
+</style>
--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -1,5 +1,5 @@
 <div class="scale-0 group-hover:scale-100 transition-transform origin-top-left
     absolute left-full top-0 w-40
-    text-left card-transparent">
+    text-left card-less-transparent z-10 pointer-events-none">
     <slot/>
 </div>
