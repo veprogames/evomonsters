285c7359f7dc7760e2bb8fef0891bebb52ed39cd
veprogames <veprogames.gamedev@gmail.com>
2022-11-22 20:05:02 +0100

    scrolling in achievement container

--- src/lib/components/tabs/TabAchievements.svelte
+++ src/lib/components/tabs/TabAchievements.svelte
@@ -6,21 +6,19 @@
     $: achievements = $game.achievements.achievements;
 </script>
 
-<div class="flex justify-center gap-4 w-full flex-wrap">
+<div class="flex justify-center gap-4 w-full flex-wrap h-80 overflow-y-scroll">
     <div class="achievement-row">
         <AchievementComp achievement={achievements.yummy} icon="/images/achievements/plate.png"></AchievementComp>
         <AchievementComp achievement={achievements.hungry} icon="/images/achievements/plate.png"></AchievementComp>
         <AchievementComp achievement={achievements.mooh} icon="/images/achievements/plate.png"></AchievementComp>
         <AchievementComp achievement={achievements.toSpace} icon="/images/achievements/plate.png"></AchievementComp>
+        <AchievementComp achievement={achievements.village} icon="/images/achievements/plate.png"></AchievementComp>
     </div>
     <div class="achievement-row">
         <AchievementComp achievement={achievements.halfADay} icon="/images/achievements/food.png"></AchievementComp>
         <AchievementComp achievement={achievements.manyDays} icon="/images/achievements/food.png"></AchievementComp>
         <AchievementComp achievement={achievements.foodOverflow} icon="/images/achievements/food.png"></AchievementComp>
         <AchievementComp achievement={achievements.endHunger} icon="/images/achievements/food.png"></AchievementComp>
-    </div>
-    <div class="achievement-row">
-        <AchievementComp achievement={achievements.village} icon="/images/achievements/plate.png"></AchievementComp>
     </div>
 </div>
 
