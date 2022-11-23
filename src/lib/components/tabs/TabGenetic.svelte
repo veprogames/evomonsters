78ac55542264385a278f1f76db6244bb96451b83
veprogames <veprogames.gamedev@gmail.com>
2022-11-23 21:09:42 +0100

    improve genetic tab

--- src/lib/components/tabs/TabGenetic.svelte
+++ src/lib/components/tabs/TabGenetic.svelte
@@ -7,11 +7,12 @@
     $: gain = $game.genetic.gain;
 </script>
 
-<div class="card-transparent flex flex-col items-center gap-2">
-    <button disabled={gain.eq(0)} on:click={() => $game.genetic.mutate()}>
+<div class="card-transparent flex flex-col items-center gap-1 h-fit">
+    <button disabled={gain.eq(0)} on:dblclick={() => $game.genetic.mutate()}>
         Mutate and gain<br/>
         <Icon src="/images/resources/genetic.png"/> <span class="text-green-400 font-semibold">{F(gain)}</span><br/>
-        (Genetic Points)
+        (Genetic Points)<br/>
+        <span class="text-green-400 font-semibold">+100%</span> Damage each
     </button>
     <p class="stat green">Damage Boost</p>
     <p class="text-xl font-semibold">x {F($game.genetic.damageBoost)}</p>
