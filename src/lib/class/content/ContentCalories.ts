4affee2f089363b51d42f0d0a5f2bee087a52b1c
veprogames <veprogames.gamedev@gmail.com>
2022-11-28 19:15:26 +0100

    talekinsesis starts at 1/s

--- src/lib/class/content/ContentCalories.ts
+++ src/lib/class/content/ContentCalories.ts
@@ -15,7 +15,7 @@ export default class ContentCalories extends GameResource implements JSONifier{
         this.upgrades = {
             telekinesis: new CaloriesUpgrade({
                 getPrice: level => Decimal.pow(3.2, level).mul(1000),
-                getEffect: level => new Decimal(level > 0 ? 0.5 + 0.15 * level : 0),
+                getEffect: level => new Decimal(level > 0 ? 0.85 + 0.15 * level : 0),
                 requiredAchievement: game.achievements.achievements.yummy,
                 title: "Telekinesis",
                 description: "Automatically bite",
