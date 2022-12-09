6b4d1187ba44e7aa980be3a74ed9d74c6e24bd5c
veprogames <veprogames.gamedev@gmail.com>
2022-12-09 15:23:55 +0100

    settings tabs

---
+++ src/lib/components/tabs/settings/TabNotations.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import gamenotations from "../../../gamenotations";
+    import { settings } from "../../../stores";
+
+    $: current = $settings.notation.name;
+</script>
+
+<div class="flex gap-2 flex-wrap">
+    {#each gamenotations as notation}
+        <button class:text-green-400={current === notation.name} on:click={() => $settings.setNotation(notation.name)}>{notation.name}</button>
+    {/each}
+</div>
---
+++ src/lib/components/tabs/settings/TabSaveLoad.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { getSaveCode, hardResetGame, loadGame, saveGame } from "../../../savegame/saveload";
+    import { game } from "../../../stores";
+
+    let importCode = "";
+    let a: HTMLAnchorElement;
+
+    let hardResetConfirmation = "";
+
+    $: saveCode = getSaveCode($game);
+    $: href = "data:text/plain;charset=utf-8," + saveCode;
+
+    function download(){
+        a.click();
+    }
+
+    function importGame(){
+        try{
+            $game = loadGame(importCode);
+            importCode = "";
+        }
+        catch(e){
+            alert("Something went wrong while importing");
+        }
+    }
+
+    function hardReset(){
+        if(hardResetConfirmation === "I really want to hard reset!"){
+            $game = hardResetGame();
+            hardResetConfirmation = "";
+        }
+    }
+</script>
+
+<div>
+    <button on:click={() => saveGame($game)}>Manually Save Game</button>
+    <button on:click={download}>Download Savegame</button>
+    <textarea class="w-full h-16" placeholder="Paste Code to import here" bind:value={importCode}></textarea>
+    <button on:click={importGame}>Import from Textbox</button>
+    <a bind:this={a} class="hidden" download="evomonsters.save.txt" {href}>Download</a>
+    <p>If you want to hard reset the Game, type 'I really want to hard reset!' into the textbox and double click 'Hard Reset'</p>
+    <input bind:value={hardResetConfirmation}/>
+    <button on:dblclick={hardReset}>Hard Reset</button>
+</div>
