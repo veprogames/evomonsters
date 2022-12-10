5580c9b6e19e950ecb3a0b1f6e7522ab50318fb4
veprogames <veprogames.gamedev@gmail.com>
2022-12-10 12:00:56 +0100

    notify settings store on notation change

--- src/lib/components/tabs/settings/TabNotations.svelte
+++ src/lib/components/tabs/settings/TabNotations.svelte
@@ -3,10 +3,18 @@
     import { settings } from "../../../stores";
 
     $: current = $settings.notation.name;
+
+    function setNotation(name: string){
+        // notify all store subscribers so the buttons get updated properly
+        settings.update(s => {
+            s.setNotation(name);
+            return s;
+        });
+    }
 </script>
 
 <div class="flex gap-2 flex-wrap">
     {#each gamenotations as notation}
-        <button class:text-green-400={current === notation.name} on:click={() => $settings.setNotation(notation.name)}>{notation.name}</button>
+        <button class:text-green-400={current === notation.name} on:click={() => setNotation(notation.name)}>{notation.name}</button>
     {/each}
 </div>
